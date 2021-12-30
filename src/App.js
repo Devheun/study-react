63e60dff7235f6aff656fcf21b3ddb43f1e04ac3
DevHeun <dudwo5600@korea.ac.kr>
2021-12-31 00:49:44 +0900

    react-practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
+import Wrapper from './Wrapper';
 
 function App(){
+  const name = 'react';
+  const style={
+    backgroundColor : 'black',
+    color : 'aqua',
+    fontSize : 24,
+    padding : '1rem'
+  }
+
   return(
-    <div>
-      <Hello />
-    </div>
-  )
+    <Wrapper>
+      <Hello name="react" color="red" isSpecial ={true}/>
+      <Hello color="pink"/>
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </Wrapper>
+  );
 }
 
 export default App;
---
+++ src/Wrapper.js
@@ -0,0 +1,16 @@
+import React, { Children } from 'react';
+
+function Wrapper(props){
+    const style={
+        border : '2px solid black',
+        padding : '16px',
+    };
+
+    return (
+        <div style={style}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Wrapper;
