3d3832dbe028fdf3c6a3a38a5d351d977543643d
DevHeun <dudwo5600@korea.ac.kr>
2022-01-01 23:54:06 +0900

    react-practice

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import React from 'react';
 import Hello from './Hello';
 import './App.css';
 import Wrapper from './Wrapper';
+import Counter from './Counter'
 
 function App(){
+  {/*
   const name = 'react';
   const style={
     backgroundColor : 'black',
     color : 'aqua',
     fontSize : 24,
     padding : '1rem'
-  }
+  */}
+
 
   return(
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial ={true}/>
-      <Hello color="pink"/>
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </Wrapper>
+    <Counter/>
+
+    //<Wrapper>
+      //<Hello name="react" color="red" isSpecial ={true}/>
+      //<Hello color="pink"/>
+      //<div style={style}>{name}</div>
+      //<div className="gray-box"></div>
+    //</Wrapper>
+  
   );
+  
 }
 
 export default App;
