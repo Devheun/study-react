ac16e7cc49428619e1f58a504c1a336e2a95050c
DevHeun <dudwo5600@korea.ac.kr>
2022-01-05 22:58:32 +0900

    add thing into array

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React,{useRef} from 'react';
+import React,{useRef,useState} from 'react';
 import Hello from './Hello';
 import './App.css';
 import Wrapper from './Wrapper';
 import Counter from './Counter'
 import InputSample from './InputSample'
 import UserList from './UserList'
+import CreateUser from './CreateUser'
 
 function App(){
-  const users=[
+  const [inputs,setInputs]=useState({
+    username:'',
+    email:''
+  });
+
+  const {username,email}=inputs;
+
+  const onChange=(e)=>{
+    const {name,value}=e.target;
+    setInputs({
+      ...inputs,
+      [name]:value
+    });
+  }
+
+  const [users,setUsers]=useState([
     {
         id:1,
         username:'siheun',
@@ -23,7 +39,7 @@ function App(){
         username:'fiers',
         email:'fiers@naver.com'
     }
-  ];
+  ]);
   {/* 
   const name = 'react';
   const style={
@@ -34,12 +50,26 @@ function App(){
   */}
   const nextId=useRef(4);
   const onCreate=()=>{
+
+    const user={
+      id : nextId.current,
+      username,
+      email
+    };
+    setUsers([...users,user]);
+
+    setInputs({
+      username:'',
+      email:''
+    });
     nextId.current+=1;
   }
 
   return(
-    
+    <>
+    <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
     <UserList users={users}/>
+    </>
     //<InputSample />
     //<Counter/>
     //<Wrapper>
